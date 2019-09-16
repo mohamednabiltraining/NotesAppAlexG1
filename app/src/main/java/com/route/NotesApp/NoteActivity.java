48b7a607b51885cf0b2072f21cf82f6ad9dbb1b5
peter <petersalahsamir@gmail.com>
2019-09-16 15:52:15 +0200

    get content from home app and set in note activity

--- app/src/main/java/com/route/NotesApp/NoteActivity.java
+++ app/src/main/java/com/route/NotesApp/NoteActivity.java
@@ -3,14 +3,17 @@ package com.route.NotesApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NoteActivity extends AppCompatActivity {
-
+    TextView noteContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        noteContent= findViewById(R.id.note_content);
         String content = getIntent().getStringExtra("content");
+        noteContent.setText(content);
     }
 }
