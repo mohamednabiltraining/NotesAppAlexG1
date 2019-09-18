c8af38639c955d66f4d81034079f25675d039023
peter <petersalahsamir@gmail.com>
2019-09-18 12:47:58 +0200

    update note adpter with new button

--- app/src/main/java/com/route/NotesApp/NotesAdapter.java
+++ app/src/main/java/com/route/NotesApp/NotesAdapter.java
@@ -3,6 +3,7 @@ package com.route.NotesApp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -44,6 +45,12 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
                 onItemClick.onClick(position);
             }
         });
+        holder.updaet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClick.onClick(position);
+            }
+        });
 
     }
 
@@ -63,6 +70,7 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
         return notes.size();
     }
     onItemClick onItemClick;
+    onItemClick onButtonClick;
 
     public void setOnItemClick(NotesAdapter.onItemClick onItemClick) {
         this.onItemClick = onItemClick;
@@ -75,10 +83,12 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
     public class  ViewHolder extends RecyclerView.ViewHolder{
         TextView title;
         TextView time;
+        Button updaet;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title=itemView.findViewById(R.id.title);
             time=itemView.findViewById(R.id.time);
+            updaet = itemView.findViewById(R.id.update);
         }
     }
 }
